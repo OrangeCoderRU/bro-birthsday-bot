cfcc18d975a44199f6e4d91eb2f23efe9fde4bf4
OrangeCoderRU <95Pahose12345>
2021-10-26 04:22:14 +0300

    fixed calculate timestamp

--- BirthDate.py
+++ BirthDate.py
@@ -15,11 +15,12 @@ def get_message_birth():
     message_string = ""
     for dates in list_of_birth:
         # timestamp = (dates[1].month - today.month) + (dates[1].day - today.day)
-        timestamp = (today - dates[1]).days - dates[1].year * 365
-        if timestamp == 0:
-            message_string += f"\nСегодня день рождения у {dates[0]}!!!\nЕму {today.year - dates[1].year}!"
-        elif timestamp in range(1, 14):
-            message_string += f"{dates[0]} через {timestamp} дней ({dates[1].month}.{dates[1].day}) \n"
-        elif timestamp in range(-14, 0):
-            message_string += f"{dates[0]} был {timestamp * (-1)} дней назад ({dates[1].month}.{dates[1].day}) \n"
+        if today.month == dates[1].month:
+            timestamp = dates[1].day - today.day
+            if timestamp == 0:
+                message_string += f"\nСегодня день рождения у {dates[0]}!!!\nЕму {today.year - dates[1].year}!"
+            elif timestamp in range(1, 14):
+                message_string += f"{dates[0]} через {timestamp} дней ({dates[1].month}.{dates[1].day}) \n"
+            elif timestamp in range(-14, 0):
+                message_string += f"{dates[0]} был {timestamp * (-1)} дней назад ({dates[1].month}.{dates[1].day}) \n"
     return message_string
